594d28b0a6035b4bd0452c86244ad7ab5d87bcde
Sri Sai Kailash <mahankalikailash25@gmail.com>
2026-04-03 14:46:15 +0530

    Update JSON files to use "CloudDefinitions" key and change API URL in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = 'https://pedabumqcd.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
+  const apiUrl = 'https://zy3plzsf3k.execute-api.us-east-1.amazonaws.com/dev'; // Replace with your API Gateway URL
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
